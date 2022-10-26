c8a01f5e6fee374d55ad99512ea8f434695e35a6
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-26 16:45:37 -0300

    Implementos header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Box, Center, Text } from '@chakra-ui/react';
+
+export const Header = () => {
+  return (
+    <Box>
+      <Center backgroundColor='orange' padding='5px'>
+        <Text fontSize='3xl'>Dio Bank</Text>
+      </Center>
+    </Box>
+  )
+}
