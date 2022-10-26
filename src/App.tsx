236a3002c5f6fd5d0ac29024f7df4cfc2a5f0580
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-26 16:46:11 -0300

    Implementos app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
-import { 
-  ChakraProvider,
-  Box,
-  Button,
-  Center,
-  Input 
-} from '@chakra-ui/react';
-import { login } from './services/login';
+import { ChakraProvider, Box} from '@chakra-ui/react';
+import { Card } from './components/Card';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <>
      <ChakraProvider>
-        <Box minHeight='100vh'backgroundColor='#7047eb' padding='25px'>
-          <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px'>
-            <Center>
-              <h1>Faça o Login</h1>
-            </Center>
-            <Input placeholder='email' />
-            <Input placeholder='password' />
-            <Center>
-              <Button onClick={login} colorScheme='teal' size='sm' width='100%' marginTop='5px'>
-                Entrar
-              </Button>
-            </Center>
+       <Layout>
+          <Box minHeight='100vh' backgroundColor='#7047eb' padding='25px'>
+            <Card />
           </Box>
-        </Box>
+        </Layout>
      </ChakraProvider> 
     </>
   );
