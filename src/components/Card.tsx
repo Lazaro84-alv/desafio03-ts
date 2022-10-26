9d81c8828f454474da401f32098fa8c0a907b383
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-26 16:42:30 -0300

    Implementos me card components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { Box, Center, Input } from '@chakra-ui/react';
+import { login } from '../services/login';
+import { DButton } from './DButton';
 
-export const Card = ({ id, paragraph, details }: ICard ) => {
-  return(
-    <>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </>
-  )
-}
+export const Card = () => {
+  return (
+    <Box backgroundColor="#FFFFFF" borderRadius="25px" padding="15px">
+      <Center>
+        <h1>Faça o login</h1>
+      </Center>
+      <Input placeholder="email" />
+      <Input placeholder="password" />
+      <Center>
+        <DButton 
+          onClick={login}
+        />
+      </Center>
+    </Box>
+  );
+};
+
+
