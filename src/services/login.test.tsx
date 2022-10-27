b660c9a0478e60d810406629ba1854a1d110969c
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-26 21:36:57 -0300

    Implementos login components e login testes

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -4,9 +4,15 @@ describe('login', () => {
 
   const mockAlert = jest.fn()
   window.alert = mockAlert
+  const mockEmail = 'picles@dio.me'
 
   it('Deve exibir um alert com boas vindas', () => {
-    login()
-    expect(mockAlert).toHaveBeenCalledWith('Bem vindo!')
+    login(mockEmail)
+    expect(mockAlert).toHaveBeenCalledWith(`Bem vindo ${mockEmail}!`)
+  })
+
+  it('Não deve exibir a mensagem de baos vindas sem o email', () => {
+    login(mockEmail)
+    expect(mockAlert).not.toHaveBeenCalledWith('Bem vindo!')
   })
 })
