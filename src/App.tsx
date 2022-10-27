784ccd658363e6c0fd089a9cc06ac0196962bb6d
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-26 21:36:08 -0300

    Implementa funcao de mouseeventhandler em dbutton components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+
 import { ChakraProvider, Box} from '@chakra-ui/react';
 import { Card } from './components/Card';
 import { Layout } from './components/Layout';
 
 function App() {
+
   return (
     <>
      <ChakraProvider>
        <Layout>
-          <Box minHeight='100vh' backgroundColor='#7047eb' padding='25px'>
+          <Box padding='25px'>
             <Card />
           </Box>
         </Layout>
@@ -18,3 +19,4 @@ function App() {
 }
 
 export default App;
+
