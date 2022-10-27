9ba3df5c40b92e121fe967cdfaa122cb0c4b00c0
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-26 21:34:21 -0300

    Implementos em layout components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,11 @@
-import { Footer } from "./Footer"
-import { Header } from "./Header"
+import { Box } from '@chakra-ui/react';
+import { Header } from './Header';
 
 export const Layout = ({ children }: any) => {
   return(
-    <>
+    <Box minHeight='100vh' backgroundColor='#7047eb'>
       <Header />
       { children }
-      <Footer />
-    </>
-  )
+    </Box>
+  );
 }
