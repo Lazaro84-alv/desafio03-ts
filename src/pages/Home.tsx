fe062ef5674249f16aba11208041ea5530995c16
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-27 16:58:00 -0300

    Implementando conta components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider, Box} from '@chakra-ui/react';
+import { Card } from '../components/Card';
+import { Layout } from '../components/Layout';
+
+const Home = () => {
+  return (
+    <Box padding='25px'>
+      <Card />
+    </Box>
+  )
+}
+
+export default Home;
