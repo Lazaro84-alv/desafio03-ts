868ffaafaa38600f61a2559b9ea4716872d0a90d
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-26 21:35:13 -0300

    Implementa funcao de onclick em card components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-
+import { useState } from 'react';
 import { Box, Center, Input } from '@chakra-ui/react';
 import { login } from '../services/login';
 import { DButton } from './DButton';
 
 export const Card = () => {
+  const [ email, setEmail ] = useState('');
+
   return (
     <Box backgroundColor="#FFFFFF" borderRadius="25px" padding="15px">
       <Center>
         <h1>Faça o login</h1>
       </Center>
-      <Input placeholder="email" />
+      <Input placeholder="email" value={email} onChange={(event) => setEmail(event.target.value)} />
       <Input placeholder="password" />
       <Center>
         <DButton 
-          onClick={login}
+          onClick={() => login(email)}
         />
       </Center>
     </Box>
